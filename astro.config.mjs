d2001665f0c2ec5428fb72b6ff8bf8b9ab50d404
toybreaker <toybreaker@users.noreply.github.com>
2023-09-01 14:21:59 +0800

    small fixes | v0.4.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,17 @@
-import { defineConfig, sharpImageService } from 'astro/config'
+import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import yaml from '@rollup/plugin-yaml' //maybe needed, currently using .json
-
 import partytown from '@astrojs/partytown'
 
 export default defineConfig({
-  image: {
-    service: sharpImageService()
-  },
   output: 'static',
   build: {
-    // The 'auto' option will inline only the stylesheets that are below the vite.build.assetsInlineLimit threshold,
-    // reducing the number of requests for smaller sheets.
-    // Larger stylesheets, such as global ones used by all pages,
-    // will still be fetched externally and cached:
-    // inlineStylesheets: 'auto'
-
-    // inline all stylesheets:
     inlineStylesheets: 'always'
-  },
-  markdown: {
-    drafts: true
   },
   integrations: [
     mdx({
       extendMarkdownConfig: false,
-      gfm: true,
-      drafts: true
+      gfm: true
     }),
     partytown()
   ],
