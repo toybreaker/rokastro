045f9c62ffb7777116c262e74bf67888fca26c0f
toybreaker <toybreaker@users.noreply.github.com>
2024-02-21 23:39:15 +0800

    new ogImages and ogCode | v1.0.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-//import partytown from '@astrojs/partytown'
 
 export default defineConfig({
   output: 'static',
   build: {
     inlineStylesheets: 'always'
   },
+  experimental: {
+    contentCollectionCache: true
+  },
   integrations: [
     mdx({
       extendMarkdownConfig: false,
       gfm: true
     })
-    //partytown()
   ],
   outDir: './dist',
   site: 'https://rokma.com/',
